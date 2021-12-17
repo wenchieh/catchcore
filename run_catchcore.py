c1171b178706a36b8a9ec351dec83909485b092b
wenchiehfeng <wenchiehfeng.us@gmail.com>
2021-12-17 19:35:28 +0800

    update

--- run_catchcore.py
+++ run_catchcore.py
@@ -38,22 +38,22 @@ from src.utils.ioutils import save_hierten_indicators
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="[CatchCore]: Catching Hierarchical Dense Sub-Tensor.",
                                      usage="python run_catchcore.py ins outs dim valcol hs p cons etas eps sep")
-    parser.add_argument("ins", help="input tensor path", type=str)
-    parser.add_argument("outs", help="result output path", type=str)
-    parser.add_argument("dim", help="the number of feature dimensions", type=int)
-    parser.add_argument("valcol", help="the column of 'measurement' in the input tensor", type=int)
-    parser.add_argument("hs", help="the expected number of hierarchies", type=int)
-    parser.add_argument("p", help="the penalty for missing entities", type=float)
-    parser.add_argument(
-        "cons", help="the lagrange parameter for constraints of optimization func.", type=float)
-    parser.add_argument("etas", help="the density ratio for two adjacent hierarchies", type=float)
-    parser.add_argument("eps", help="the convergence parameter", type=float)
-    parser.add_argument("sep", help="separator of input tensor", type=str, default=' ')
+    parser.add_argument("--ins",  help="input tensor path", type=str, default="./example.tensor")
+    parser.add_argument("--outs", help="result output path", type=str, default="./output/")
+    parser.add_argument("--dim",  help="the number of feature dimensions", type=int, default=3)
+    parser.add_argument("--valcol", help="the column of 'measurement' in the input tensor", type=int, default=-1)
+    parser.add_argument("--hs",   help="the expected number of hierarchies", type=int, default=2)
+    parser.add_argument("--p",    help="the penalty for missing entities", type=float, default=3e-4)
+    parser.add_argument("--cons", help="the lagrange parameter for constraints of optimization func.", type=float, default=20)
+    parser.add_argument("--etas", help="the density ratio for two adjacent hierarchies", type=float, default=10)
+    parser.add_argument("--eps",  help="the convergence parameter", type=float, default=1e-6)
+    parser.add_argument("--sep",  help="separator of input tensor", type=str, default=',')
     args = parser.parse_args()
 
     ins, sep, outs = args.ins, args.sep, args.outs
     hs, p, cons, etas, eps = args.hs, args.p, args.cons, args.etas, args.eps
     dims, valcol = args.dim, args.valcol
+
     outfn = 'hierways.out'
     print("\nhs:{}\npenalty: {}\netas: {}\nconstraint:{}\neps:{}\n".format(hs, p, etas, cons, eps))
 
